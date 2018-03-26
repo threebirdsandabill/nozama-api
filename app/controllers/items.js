8e55bf30c40c9b7bef1d58ce70a0be16d72d4590
HTarzwell <htarzwell@gmail.com>
2018-03-26 11:16:29 -0400

    Added new file to controllers + edited config routes
    - new file items.js in controllers
    - deleted all routes except index and show
    - added items to routes

---
+++ app/controllers/items.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const controller = require('lib/wiring/controller')
+const models = require('app/models')
+const Item = models.item
+
+const setModel = require('./concerns/set-mongoose-model')
+
+const index = (req, res, next) => {
+  Item.find()
+    .then(items => res.json({
+      items: items.map((e) =>
+        e.toJSON({ virtuals: true }))
+    }))
+    .catch(next)
+}
+
+const show = (req, res) => {
+  res.json({
+    item: req.item.toJSON({ virtuals: true })
+  })
+}
+
+module.exports = controller({
+  index,
+  show,
+}, { before: [
+  { method: setUser, only: ['index', 'show'] },
+  { method: authenticate, except: ['index', 'show'] },
+  { method: setModel(Item), only: ['show'] }
+] })
