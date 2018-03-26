6e345e980440dfb2082be47c48272e2e893170a3
HTarzwell <htarzwell@gmail.com>
2018-03-26 11:10:25 -0400

    Added 'item.js' model to Models
    -populated file from Example.js

---
+++ app/models/item.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  image: {
+    type: String,
+    required: tru
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: function (doc, ret, options) {
+      const userId = (options.user && options.user._id) || false
+      ret.editable = userId && userId.equals(doc._owner)
+      return ret
+    }
+  }
+})
+
+itemSchema.virtual('length').get(function length () {
+  return this.text.length
+})
+
+const Item = mongoose.model('Item', itemSchema)
+
+module.exports = Item
